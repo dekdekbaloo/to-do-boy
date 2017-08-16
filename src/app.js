0c3f9698d2a1039b694b7da828a1bb31d3b70cbd
aunaun <dekdekbaloo@gmail.com>
2017-08-17 00:39:55 +0700

    read env vars

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 import express from 'express'
 import morgan from 'morgan'
+require('dotenv').config()
 const app = express()
 app.use(morgan('tiny'))
 app.get('/webhook', (req, res) => {
-  res.sendStatus(200)
+  if (req.query['hub.mode'] === 'subscribe' &&
+    req.query['hub.verify_token'] === process.env.FB_VERIFY_TOKEN) {
+    console.log('Validating webhook')
+    res.status(200).send(req.query['hub.challenge'])
+  } else {
+    console.error('Failed validation. Make sure the validation tokens match.')
+    res.sendStatus(403)
+  }
 })
 
 const port = process.env.PORT || 3000
