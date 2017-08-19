651fabfcabebd695d26ce52a83d62557f2316b94
aunaun <dekdekbaloo@gmail.com>
2017-08-20 01:38:57 +0700

    Using express middleware!

--- scripts/packFunctions.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf functions && mkdir functions
-cp -rfv build/* functions
-cp package.json functions
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'functions'),
+    filename: 'app.js',
+    libraryTarget: 'commonjs2'
+  },
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: path.resolve(__dirname, 'node_modules'),
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        }]
+      }
+    ]
+  }
+}
